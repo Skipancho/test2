5438026efc979b3a3fa0e53b7633de222e43e8a0
Skipancho <ctg1770a@gmail.com>
2021-09-07 23:13:34 +0900

    html_to_php

---
+++ header.php
@@ -0,0 +1,6 @@
+<?php
+    header('Content-Type: application/json;charset=UTF-8');
+    header("HTTP/1.1 200 OK");
+    header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE"); 
+    $time = date("Y-m-d H시i분");
+?>
